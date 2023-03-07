9985713e73faa6ecf7376eb39d49e402fd703476
ryzhy1 <skirlyak2004@mail.ru>
2023-03-07 20:16:57 +0300

    1491 and 191

--- easy/191_Number_of_1_Bits/file.ts
+++ easy/191_Number_of_1_Bits/file.ts
@@ -1,12 +1,5 @@
-function hammingWeight(i: number): number {
-    var count = 0;
-    i = i - ((i >> 1) & 0x55555555);
-    i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
-    i = (i + (i >> 4)) & 0x0f0f0f0f;
-    i = i + (i >> 8);
-    i = i + (i >> 16);
-    count += i & 0x3f;
-    return count;
+function hammingWeight(n: number): number {
+    return n.toString(2).match(/1/g)?.length ?? 0;
 };
 
 console.log(hammingWeight(0b00000000000000000000000000001011));
