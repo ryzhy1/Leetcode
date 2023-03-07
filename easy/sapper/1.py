b916f1cbef7e0c2e633e64f99fbca34b6ef43f08
ryzhy1 <skirlyak2004@mail.ru>
2023-03-07 15:45:13 +0300

    a bit of changes

--- easy/sapper/1.py
+++ easy/sapper/1.py
@@ -41,39 +41,6 @@ def print_board(board: List[List[Union[int, str]]]) -> None:
     for row in board:
         print(tuple(row))
 
-# def create_board(rows: int, cols: int, mines: int, first_x: int, first_y: int) -> List[List[Union[int, str]]]:
-#     # создаем пустое поле
-#     board = [[0 for _ in range(cols)] for _ in range(rows)]
-
-#     # расставляем мины
-#     mine_positions = set()
-#     while len(mine_positions) < mines:
-#         x, y = random.randint(0, rows-1), random.randint(0, cols-1)
-#         if (x, y) != (first_x, first_y):
-#             mine_positions.add((x, y))
-#             board[x][y] = 'X'
-
-#     # рассчитываем количество мин вокруг каждой клетки
-#     for i in range(rows):
-#         for j in range(cols):
-#             if board[i][j] != 'X':
-#                 count = 0
-#                 for di in range(-1, 2):
-#                     for dj in range(-1, 2):
-#                         if 0 <= i+di < rows and 0 <= j+dj < cols and board[i+di][j+dj] == 'X':
-#                             count += 1
-#                 board[i][j] = count
-
-#     # открываем первую клетку
-#     board[first_x][first_y] = '0'
-
-#     return board
-
-
-# def print_board(board: List[List[Union[int, str]]]) -> None:
-#     for row in board:
-#         print(tuple(row))
-
 rows = int(input("Введите количество строк: "))
 cols = int(input("Введите количество столбцов: "))
 mines = int(input("Введите количество мин: "))
